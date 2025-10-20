48546c1d4e3c2868bd479263de79ff736673f94a
Julian Aryo <julian.aryo1989@gmail.com>
2025-10-20 10:42:48 +0800

    WIP

--- scripts/character.gd
+++ scripts/character.gd
@@ -4,7 +4,26 @@ class_name Character extends Node
 @export var damage := 10
 @export var speed := 150.0
 @export var body : CharacterBody2D
+@export var sprite : AnimatedSprite2D
 @export var animation : AnimationPlayer
 
-func action():
-	pass
+func process(direction : Vector2, isAttacking : bool):
+	if isAttacking:
+		body.velocity = Vector2.ZERO
+		animation.play('attack')
+		return
+	
+	if animation.current_animation != 'attack':
+		if direction.x > 0:
+			sprite.scale.x = 1
+		elif direction.x < 0:
+			sprite.scale.x = -1
+			
+		if direction != Vector2.ZERO:
+			body.velocity = direction * speed
+			animation.play('run')
+		else:
+			body.velocity = Vector2.ZERO
+			animation.play('idle')
+		
+		body.move_and_slide()
--- scripts/swordman.gd
+++ scripts/swordman.gd
@@ -1,11 +1,10 @@
 class_name Swordman extends Character
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var isAttacking := false
+
 func _process(delta: float) -> void:
 	var direction = Vector2(Input.get_axis('left','right'), Input.get_axis('up','down')).normalized()
-	body.velocity = direction * speed
-	body.move_and_slide()
-	pass
+	process(direction, isAttacking)
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
-	pass
+	isAttacking = true
